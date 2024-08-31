dba536e68d32f4ef6eafe294902e1445dec2d4e9
JhonChilo <jhon.chilo@utec.edu.pe>
2024-08-31 17:50:34 -0500

    10

--- docs/main.py
+++ docs/main.py
@@ -2,13 +2,21 @@ import ast
 import operator as op
 
 # Supported operators
-operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul, ast.Div: op.truediv}
+operators = {
+    ast.Add: op.add, 
+    ast.Sub: op.sub, 
+    ast.Mult: op.mul, 
+    ast.Div: op.truediv,
+    ast.USub: op.neg  # Handle unary subtraction
+}
 
 def evaluate_expr(node):
     if isinstance(node, ast.Num):  # <number>
         return node.n
     elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
         return operators[type(node.op)](evaluate_expr(node.left), evaluate_expr(node.right))
+    elif isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
+        return operators[type(node.op)](evaluate_expr(node.operand))
     else:
         raise TypeError(node)
 
